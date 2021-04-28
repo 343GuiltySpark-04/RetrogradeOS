5920004c8b4b481fc9264f65f0be39e84c257f3a
Tristan Adams <tristan@devbot.example.net>
2021-04-28 19:01:52 -0400

    ressetting

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -5,31 +5,17 @@
 
 
 
-static inline unsigned long read_cr0(void)
-{
-    unsigned long val;
-    asm volatile ( "mov %%cr0, %0" : "=r"(val) );
-    return val;
-}
+
 
 void kernel_main(void) {
 	terminal_initialize();
 
-	int aif = 1;
+	
 
-	if (aif == 1){
+	printf("GUIDANCE SYSTEMS: CHECK!\n");
+    printf("STARTING PRE-LAUNCH CHECK....\n");
 
-		printf("TEST 1");
-
-	}else{
-
-		printf("TEST 2");
-	}
-
-	//printf("GUIDANCE SYSTEMS: CHECK!\n");
-   // printf("STARTING PRE-LAUNCH CHECK....\n");
-
-	printf(read_cr0);
+	
 
 	
 
