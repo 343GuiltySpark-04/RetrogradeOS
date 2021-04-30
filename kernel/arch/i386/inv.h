324f3381ae8e96a94be1cb41269ed48d44cfa3eb
Tristan Adams <tristan@devbot.example.net>
2021-04-30 09:08:14 -0400

    dada

---
+++ kernel/arch/i386/inv.h
@@ -0,0 +1,15 @@
+#ifndef INB
+#define INB
+
+#include <stdint.h>
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+#endif
