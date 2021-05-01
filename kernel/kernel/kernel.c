1f61d60f00b5f71874d84ac3e10e4b8f3706d67d
Tristan Adams <tristan@devbot.example.net>
2021-04-30 21:12:22 -0400

    added a check and abort for the itureputs

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <kernel/check_sti.h>
 #include <kernel/tty.h>
+#include <stdlib.h>
 
 void kernel_main(void)
 {
 	terminal_initialize();
+
+	bool check_interrupts = are_interrupts_enabled();
+
+	if (check_interrupts == true)
+	{
+
+		printf("INTERRUPTS SUBSYSTEM: CHECK!");
+	}
+	else if (check_interrupts == false)
+	{
+		printf("ERROR: PROBLEM WITH ITERRUPTS SUBYSTEM! ABORT LAUNCH!");
+		abort();
+	}
 
 	printf("GUIDANCE SYSTEMS: CHECK!");
 	printf("STARTING PRE-LAUNCH CHECK....");
