3664b0cc3481e80f10d6f636c646d444ccba1060
Tristan Adams <tristan@devbot.example.net>
2021-05-09 11:26:16 -0400

    Now to do the asm bit

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -304,7 +304,7 @@ void main()
 		abort();
 	}
 
-	// Setup Paging
+	// Create Paging Directory
 
 	uint32_t page_directory[1024] __attribute__((aligned(4096)));
 
@@ -317,6 +317,27 @@ void main()
 		//   Not Present: The page table is not present
 		page_directory[i] = 0x00000002;
 	}
+
+	// Create First Page Table
+
+	uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
+	// holds the physical address where we want to start mapping these pages to.
+	// in this case, we want to map these pages to the very beginning of memory.
+	unsigned int t;
+
+	//we will fill all 1024 entries in the table, mapping 4 megabytes
+	for (t = 0; t < 1024; t++)
+	{
+		// As the address is page aligned, it will always leave 12 bits zeroed.
+		// Those bits are used by the attributes ;)
+		first_page_table[t] = (t * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+	}
+
+	// Put the table in the directory
+
+	// attributes: supervisor level, read/write, present
+	page_directory[0] = ((unsigned int)first_page_table) | 3;
 
 	/* prim_wait(1000);
 	currently not working right */
