95a091f177b3389f4f102b82e9425199aa4663bd
Tristan Adams <tristan@devbot.example.net>
2021-05-12 21:40:49 -0400

    dadaldkoa

--- kernel/arch/i386/prim_wait.c
+++
@@ -1,8 +0,0 @@
-#include <kernel/prim_wait.h>
-
-// TODO: Fine tune it or find a way to scale it to the cpu or sync to its clock better.
-volatile void prim_wait(unsigned int millis)
-{
-    for(unsigned int i = 0; i < millis; i++)
-        for(unsigned int j = 0; j < _PRIM_SECOND; j++);
-}
--- kernel/include/kernel/prim_wait.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PRIM_WAIT
-#define _PRIM_WAIT
-#define _PRIM_SECOND 550000000
-
-#include <stdio.h>
-
-volatile void prim_wait(unsigned int millis);
-
-#endif
