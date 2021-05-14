7cb8a09e7ea0f9bdfaa654c285911dc1c8659e70
Tristan Adams <tristan@devbot.example.net>
2021-05-14 12:12:17 -0400

    new file:   changelog.md 	modified:   release.sh

---
+++ changelog.md
@@ -0,0 +1,8 @@
+
+# Version: 0.0.1b
+- added a command to display the version
+
+
+
+# Version: 0.0.1
+- inital release
--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ mkdir -v ./release
 cp -v retrogradeOS.iso ./release
 cp -v LICENSE ./release
 cp -v README.md ./release
+cp -v changelog.md ./release
 tar -vcf ./RetrogradeOS.tar ./release/*
 mv -v ./RetrogradeOS.tar ./RetrogradeOS-$VERSION.tar
 gzip -v -5 ./RetrogradeOS-$VERSION.tar
