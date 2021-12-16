c8238e8bdd37c069e51c94d7f1bbd0f3fe429e4a
Tristan Adams <killerdragonxs@gmail.com>
2021-12-16 10:03:01 -0500

    Create common.h

---
+++ libc/include/common.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// standard typedefs 
+
+typedef unsigned int    u32int;
+typedef          int    s32int;
+typedef unsigned short  u16int;
+typedef          short  s16int;
+typedef unsigned char   u8int;
+typedef          char   s8int;
+
+
+
+
+
+
+
+
+
+
+#endif
