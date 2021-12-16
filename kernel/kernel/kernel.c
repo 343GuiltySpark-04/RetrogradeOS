70791297afb84b4c2e8235bfeaf9d3f2f195adb9
Tristan Adams <killerdragonxs@gmail.com>
2021-12-16 11:09:15 -0500

    lkjhg

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -402,6 +402,23 @@ void paging()
 		//   Not Present: The page table is not present
 		page_directory[i] = 0x00000002;
 	}
+
+	u32int first_page_table[1024] __attribute__((aligned(4096)));
+
+	// holds the physical address where we want to start mapping these pages to.
+	// in this case, we want to map these pages to the very beginning of memory.
+	unsigned int i2;
+
+	//we will fill all 1024 entries in the table, mapping 4 megabytes
+	for (i2 = 0; i2 < 1024; i2++)
+	{
+		// As the address is page aligned, it will always leave 12 bits zeroed.
+		// Those bits are used by the attributes ;)
+		first_page_table[i2] = (i2 * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+	}
+
+	// attributes: supervisor level, read/write, present
+	page_directory[0] = ((unsigned int)first_page_table) | 3;
 }
 
 // ----- Entry point -----
