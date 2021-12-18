73afa7981b6509d086b72a04af28dd2f4b9d9d7f
Tristan Adams <killerdragonxs@gmail.com>
2021-12-18 14:14:24 -0500

    Update common.h

--- libc/include/common.h
+++ libc/include/common.h
@@ -9,6 +9,8 @@ typedef unsigned short u16int;
 typedef short s16int;
 typedef unsigned char u8int;
 typedef char s8int;
+typedef unsigned long u64int;
+typedef long s64int;
 
 void outb(u16int port, u8int value);
 u8int inb(u16int port);
