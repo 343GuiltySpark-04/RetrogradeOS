f02b98834a35a481fc34ea6b80b7aa2b55b403a8
Tristan Adams <killerdragonxs@gmail.com>
2021-12-19 12:05:03 -0500

    dadad

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -444,6 +444,42 @@ void paging()
 	enablePaging();
 }
 
+void mem(multiboot_info_t* mbd, u32int magic)
+{
+    /* Make sure the magic number matches for memory mapping*/
+    if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+		write_debug_code('0', '0', '5');
+        abort();
+    }
+ 
+    /* Check bit 6 to see if we have a valid memory map */
+    if(!(mbd->flags >> 6 & 0x1)) {
+        write_debug_code('0', '0', '6');
+		abort();
+    }
+ 
+    /* Loop through the memory map and display the values */
+    int i;
+    for(i = 0; i < mbd->mmap_length; 
+        i += sizeof(multiboot_memory_map_t)) 
+    {
+        multiboot_memory_map_t* mmmt = 
+            (multiboot_memory_map_t*) (mbd->mmap_addr + i);
+ 
+        printf("Start Addr: %x | Length: %x | Size: %x | Type: %d\n",
+            mmmt->addr, mmmt->len, mmmt->size, mmmt->type);
+ 
+        if(mmmt->type == MULTIBOOT_MEMORY_AVAILABLE) {
+            /* 
+             * Do something with this memory block!
+             * BE WARNED that some of memory shown as availiable is actually 
+             * actively being used by the kernel! You'll need to take that
+             * into account before writing to memory!
+             */
+        }
+    }
+}
+
 // ----- Entry point -----
 void main()
 {
