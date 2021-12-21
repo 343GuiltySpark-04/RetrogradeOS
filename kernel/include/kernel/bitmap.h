5424a1c799a98056bd4f4d48cd555bf4573e7a9c
Tristan Adams <killerdragonxs@gmail.com>
2021-12-21 18:27:37 -0500

    Update bitmap.h

--- kernel/include/kernel/bitmap.h
+++ kernel/include/kernel/bitmap.h
@@ -28,4 +28,47 @@ inline bool bitmap_get(bitmap_t *bitmap, uint32_t index)
     return ((bitmap->buffer[byteIndex] & bitIndexer) > 0);
 }
 
+inline bool bitmap_set(bitmap_t *bitmap, uint32_t index, bool value)
+{
+    if (index > bitmap->size * 8)
+    {
+        return false;
+    }
+
+    uint32_t byteIndex = index / 8;
+    uint8_t bitIndex = index % 8;
+    uint8_t bitIndexer = 0b10000000 >> bitIndex;
+
+    bitmap->buffer[byteIndex] &= ~bitIndexer;
+
+    if (value)
+    {
+        bitmap->buffer[byteIndex] |= bitIndexer;
+    }
+
+    return true;
+}
+
+inline uint8_t GetBit(bitmap_t *bitmap, size_t bit)
+{
+    size_t bitmapIndex = bit / 4;
+    size_t bitIndex = bit % 4;
+
+    return bitmap->buffer[bitmapIndex] & (1 << bitIndex);
+}
+
+inline void SetBit(bitmap_t *bitmap, size_t bit, uint8_t value)
+{
+    size_t bitmapIndex = bit / 4;
+    size_t bitIndex = bit % 4;
+
+    if (value)
+    {
+        bitmap->buffer[bitmapIndex] |= (1 << bitIndex);
+    }
+    else
+    {
+        bitmap->buffer[bitmapIndex] &= ~(1 << bitIndex);
+    }
+
 #endif
