2294082242bdfe23f83283031a566cc9c11878c7
Tristan Adams <killerdragonxs@gmail.com>
2021-12-21 17:56:09 -0500

    Update bitmap.h

--- kernel/include/kernel/bitmap.h
+++ kernel/include/kernel/bitmap.h
@@ -4,9 +4,28 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef struct
+{
 
+    size_t size;
+    uint8_t *buffer;
 
+    /* data */
 
+} bitmap_t;
 
+inline bool bitmap_get(bitmap_t *bitmap, uint32_t index)
+{
+    if (index > bitmap->size * 4)
+    {
+        return false;
+    }
+
+    uint32_t byteIndex = index / 4;
+    uint8_t bitIndex = index % 4;
+    uint8_t bitIndexer = 0b1000 >> bitIndex;
+
+    return ((bitmap->buffer[byteIndex] & bitIndexer) > 0);
+}
 
 #endif
